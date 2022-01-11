3f5df56e577e234c0465b377a99490280dd9efe5
EC2 Default User <ec2-user@ip-172-31-52-97.ec2.internal>
2022-01-11 19:29:12 +0000

    first push

---
+++ 042_Accessor&Mutator_Methods/universal_base_code/starter.java
@@ -0,0 +1,17 @@
+import pkg.*;
+import java.util.Scanner;
+import java.util.Random;
+
+
+class starter {
+	public static void main(String args[]) {
+		// Your code goes below here
+		Random rand = new Random();
+		Dwarf[] olaf = new Dwarf[100];
+		while(true){
+		int x = rand.nextInt(7);
+		}
+
+		
+	}
+}
